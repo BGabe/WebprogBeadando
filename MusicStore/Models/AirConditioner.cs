63bdea87eebf02041e024cd3d7fd4bc060bf556b
Bese Gabor <gaborbese11@gmail.com>
2020-12-05 13:41:32 +0100

    Constructor for HouseHoldAppliances Class and Refeactoring of AirConditioner

--- MusicStore/Models/AirConditioner.cs
+++ MusicStore/Models/AirConditioner.cs
@@ -10,7 +10,7 @@ namespace MusicStore.Models
         public int CoolPower { get; set; }
         public int RecommendSpace { get; set; }
         public int AirTransport { get; set; }
-        public AirConditioner(int coolpower, int recommend, int airtransport)
+        public AirConditioner(double Price,string EnergyClass, int Width, int Deepness, int NoiseLevel, string Color, string Type, int Guarantee, int EnergyConsumption, int coolpower, int recommend, int airtransport) : base(EnergyClass, Width, Deepness, NoiseLevel, Color, Type,  Guarantee, EnergyConsumption,Price)
         {
             CoolPower = coolpower;
             RecommendSpace = recommend;
--- MusicStore/Models/HouseHoldAppliances.cs
+++ MusicStore/Models/HouseHoldAppliances.cs
@@ -12,30 +12,44 @@ namespace MusicStore.Models
     
    abstract public class HouseHoldAppliances
     {
-        
+        [DisplayName("EnergyClass")]
         public string EnergyClass { get; set; }
-        [DisplayName("Type")]
+        [DisplayName("Height")]
         public int Height { get; set; }
-
-        [DisplayName("Artist")]
+        [DisplayName("Width")]
         public int Width { get; set; }
+        [DisplayName("Deepness")]
         public int Deepness { get; set; }
+        [DisplayName("NoiseLevel")]
         public int NoiseLevel { get; set; }
-        public string Colors { get; set; }
+        [DisplayName("Colors")]
+        public string Color { get; set; }
+        [DisplayName("Type")]
         public string Type { get; set; }
+        [DisplayName("Guarantee")]
         public int Guarantee { get; set; }
+        [DisplayName("EnergyConsumption")]
         public int EnergyConsumption { get; set; }
-
-        [Required(ErrorMessage = "An Album Title is required")]
-        [StringLength(160)]
-        
         [Required(ErrorMessage = "Price is required")]
         [Range(0.01, 100.00, ErrorMessage = "Price must be between 0.01 and 100.00")]
-        public decimal Price { get; set; }
-        [DisplayName("Album Art URL")]
-        [StringLength(1024)]
+        public double Price { get; set; }
+        /*
+        [Required(ErrorMessage = "An Album Title is required")]
+        [StringLength(160)]*/
         
-       
         public virtual List<OrderDetail> OrderDetails { get; set; }
+
+        public HouseHoldAppliances(string EnergyClass,int Width, int Deepness, int NoiseLevel, string Color, string Type, int Guarantee, int EnergyConsumption, double Price)
+        {
+            this.EnergyClass = EnergyClass;
+            this.Width = Width;
+            this.Deepness = Deepness;
+            this.NoiseLevel = NoiseLevel;
+            this.Color = Color;
+            this.Type = Type;
+            this.Guarantee = Guarantee;
+            this.EnergyConsumption = EnergyConsumption;
+            this.Price = Price;
+        }
     }
 }
