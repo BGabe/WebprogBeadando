be816d9a7a1c593227ba8c954f030ed19c87f3a7
Bese Gabor <gaborbese11@gmail.com>
2020-12-06 00:51:42 +0100

    Swap existing Solution and make initial commit for a new Solution

---
+++ HouseHoldApplianceStore/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.CreateProducts;
+using Shop.Application.GetProducts;
+using Shop.Database;
+
+namespace HouseHoldApplianceStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private ApplicationDbContext _ctx;
+
+        public IndexModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [BindProperty]
+        public Shop.Application.CreateProducts.ProductViewModel Product { get; set; }
+
+        public IEnumerable<Shop.Application.GetProducts.ProductViewModel> Products { get; set; }
+
+        public void OnGet()
+        {
+            Products =  new GetProducts(_ctx).Do();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_ctx).Do(Product);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
