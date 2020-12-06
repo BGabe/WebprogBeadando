559bd14be25c9daf5531ad62d495a7ca196086c4
Bese Gabor <gaborbese11@gmail.com>
2020-12-06 01:31:02 +0100

    Creating Admin Panel Functionality

---
+++ HouseHoldApplianceStore/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.ProductsAdmin;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseHoldApplianceStore.Controllers
+{
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _ctx;
+
+        public AdminController(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetProdutcs() => Ok(new GetProducts(_ctx).Do());
+
+        [HttpGet("products/(id)")]
+        public IActionResult GetProdutc(int id) => Ok(new GetProduct(_ctx).Do(id));
+
+        [HttpPost("products")]
+        public IActionResult CreateProduct(CreateProduct.ProductViewModel vm) => Ok(new CreateProduct(_ctx).Do(vm));
+
+        [HttpDelete("products/(id)")]
+        public IActionResult DeleteProduct(int id) => Ok(new DeleteProduct(_ctx).Do(id));
+
+        [HttpPut("products")]
+        public IActionResult UpdateProduct(UpdateProduct.ProductViewModel vm) => Ok(new UpdateProduct(_ctx).Do(vm));
+    }
+}
--- HouseHoldApplianceStore/Pages/Index.cshtml.cs
+++ HouseHoldApplianceStore/Pages/Index.cshtml.cs
@@ -3,8 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Shop.Application.CreateProducts;
-using Shop.Application.GetProducts;
+using Shop.Application.Products;
 using Shop.Database;
 
 namespace HouseHoldApplianceStore.Pages
@@ -17,21 +16,13 @@ namespace HouseHoldApplianceStore.Pages
         {
             _ctx = ctx;
         }
-        [BindProperty]
-        public Shop.Application.CreateProducts.ProductViewModel Product { get; set; }
 
-        public IEnumerable<Shop.Application.GetProducts.ProductViewModel> Products { get; set; }
+
+        public IEnumerable<GetProducts.ProductViewModel> Products { get; set; }
 
         public void OnGet()
         {
             Products =  new GetProducts(_ctx).Do();
-        }
-
-        public async Task<IActionResult> OnPost()
-        {
-            await new CreateProduct(_ctx).Do(Product);
-
-            return RedirectToPage("Index");
         }
     }
 }
